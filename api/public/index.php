8eae2a0c96e7f4f67692a816b2d3745f924b2761
felipecapeletti <dev.fgerardi@gmail.com>
2024-10-02 16:10:17 -0300

    teste host

--- api/public/index.php
+++ api/public/index.php
@@ -10,8 +10,12 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../api/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../api/bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$request = Request::capture();
+$response = $app->handle($request);
+$response->send();
+$app->terminate($request, $response);
